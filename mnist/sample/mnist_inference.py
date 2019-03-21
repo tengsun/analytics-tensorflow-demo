91bbf9954ca9e80f67272d7f6ddd4327384f5f1c
tengsun <tengsun@cn.ibm.com>
2019-03-21 16:12:55 +0800

    refine inference function

--- mnist/sample/mnist_inference.py
+++ mnist/sample/mnist_inference.py
@@ -5,7 +5,7 @@ INPUT_NODE = 784
 OUTPUT_NODE = 10
 LAYER1_NODE = 500
 
-# get variable created during train
+# init weights for training
 def get_weight_variable(shape, regularizer):
     weights = tf.get_variable('weights', shape, 
         initializer=tf.truncated_normal_initializer(stddev=0.1))
@@ -14,20 +14,24 @@ def get_weight_variable(shape, regularizer):
         tf.add_to_collection('losses', regularizer(weights))
     return weights
 
+# init biases for training
+def get_bias_variable(shape):
+    biases = tf.get_variable('biases', shape, 
+            initializer=tf.constant_initializer(0.0))
+    return biases
+
 # calculate forward propagation result
 def inference(input_tensor, regularizer):
     # define the layer1 variables
     with tf.variable_scope('layer1'):
         weights = get_weight_variable([INPUT_NODE, LAYER1_NODE], regularizer)
-        biases = tf.get_variable('biases', [LAYER1_NODE], 
-            initializer=tf.constant_initializer(0.0))
+        biases = get_bias_variable([LAYER1_NODE])
         layer1 = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
     
     # define the layer2 variables
     with tf.variable_scope('layer2'):
         weights = get_weight_variable([LAYER1_NODE, OUTPUT_NODE], regularizer)
-        biases = tf.get_variable('biases', [OUTPUT_NODE], 
-            initializer=tf.constant_initializer(0.0))
+        biases = get_bias_variable([OUTPUT_NODE])
         layer2 = tf.matmul(layer1, weights) + biases
     
     return layer2
