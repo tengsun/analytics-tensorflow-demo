e87bd0bff36ed9870f8896d519a6281730bcffd7
tengsun <tengsun@cn.ibm.com>
2019-04-01 21:28:51 +0800

    add queue operation

--- img/multi_threads.py
+++ img/multi_threads.py
@@ -15,7 +15,7 @@ def MyLoop(coord, worker_id):
         time.sleep(1)
 
 
-def main():
+def multi_threads_test():
     coord = tf.train.Coordinator()
     threads = [threading.Thread(target=MyLoop, args=(coord,i,)) for i in range(5)]
     for t in threads:
@@ -23,5 +23,28 @@ def main():
     coord.join(threads)
 
 
+def queue_op_test():
+    queue = tf.FIFOQueue(100, "float")
+    enqueue_op = queue.enqueue([tf.random_normal([1])])
+    dequeue_op = queue.dequeue()
+
+    # starts 5 threads with enqueue_op operation on queue
+    queue_runner = tf.train.QueueRunner(queue, [enqueue_op] * 5)
+    tf.train.add_queue_runner(queue_runner)
+
+    with tf.Session() as sess:
+        # start queue runner
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        # print 3 values in queue
+        for _ in range(3):
+            print(sess.run(dequeue_op)[0])
+        
+        coord.request_stop()
+        coord.join(threads)
+
+
 if __name__ == '__main__':
-    main()
+    # multi_threads_test()
+    queue_op_test()
